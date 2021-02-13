308dc4fb22df8dd0d9913d9053c87c873341ac90
SidewinderDX <d_kohl@hotmail.de>
2021-02-13 11:09:25 +0100

    initial commit

---
+++ UG.py
@@ -0,0 +1,411 @@
+class UG:
+    def __init__(self):
+        self.data = {
+            "FIRST_POINT" : {
+                "x" : 0,
+                "y" : 0,
+                "z" : 0,
+                "dimension" : "minecraft:overworld",
+                "world" : "world"
+            },
+            "SECOND_POINT" : {
+                "x" : 0,
+                "y" : 0,
+                "z" : 0,
+                "dimension" : "minecraft:overworld",
+                "world" : "world"
+            },
+            "PRIORITY" : 0,
+            "TELEPORT_LOCATION" : None,
+            "SPAWN_LOCATION" : None,
+            "MEMBERS" : [],
+            "FAREWELL_MESSAGE": "",
+            "GREETING_MESSAGE": "",
+            "EFFECTS": [],
+            "SOLD": False,
+            "VALUE": None,
+            "VERSION": 2.32,
+            "ID": "",
+            "TYPE": "LOCAL",
+            "NAME": "",
+            "GAMEMODE": "minecraft:not_set",
+            "FLAGS": [
+                {
+                "name": "place",
+                "value": False
+                },
+                {
+                "name": "destroy",
+                "value": False
+                },
+                {
+                "name": "pvp",
+                "value": True
+                },
+                {
+                "name": "expdrop",
+                "value": True
+                },
+                {
+                "name": "itemdrop",
+                "value": True
+                },
+                {
+                "name": "itempickup",
+                "value": True
+                },
+                {
+                "name": "enderpearl",
+                "value": True
+                },
+                {
+                "name": "sleep",
+                "value": True
+                },
+                {
+                "name": "lighter",
+                "value": False
+                },
+                {
+                "name": "chests",
+                "value": False
+                },
+                {
+                "name": "trappedchests",
+                "value": False
+                },
+                {
+                "name": "waterflow",
+                "value": True
+                },
+                {
+                "name": "lavaflow",
+                "value": True
+                },
+                {
+                "name": "otherliquidsflow",
+                "value": True
+                },
+                {
+                "name": "leafdecay",
+                "value": True
+                },
+                {
+                "name": "firespread",
+                "value": True
+                },
+                {
+                "name": "potionsplash",
+                "value": True
+                },
+                {
+                "name": "falldamage",
+                "value": True
+                },
+                {
+                "name": "cantp",
+                "value": False
+                },
+                {
+                "name": "canspawn",
+                "value": False
+                },
+                {
+                "name": "hunger",
+                "value": True
+                },
+                {
+                "name": "enderchests",
+                "value": False
+                },
+                {
+                "name": "walldamage",
+                "value": True
+                },
+                {
+                "name": "drown",
+                "value": True
+                },
+                {
+                "name": "invincible",
+                "value": False
+                },
+                {
+                "name": "cactusdamage",
+                "value": True
+                },
+                {
+                "name": "firedamage",
+                "value": True
+                },
+                {
+                "name": "endermangrief",
+                "value": True
+                },
+                {
+                "name": "enderdragonblockdamage",
+                "value": True
+                },
+                {
+                "name": "hidelocation",
+                "value": False
+                },
+                {
+                "name": "hideflags",
+                "value": False
+                },
+                {
+                "name": "hidemembers",
+                "value": False
+                },
+                {
+                "name": "hideregion",
+                "value": False
+                },
+                {
+                "name": "icemelt",
+                "value": True
+                },
+                {
+                "name": "exit",
+                "value": True
+                },
+                {
+                "name": "enter",
+                "value": True
+                },
+                {
+                "name": "vinesgrowth",
+                "value": True
+                },
+                {
+                "name": "sendchat",
+                "value": True
+                },
+                {
+                "name": "trample",
+                "value": True
+                },
+                {
+                "name": "shulkerboxes",
+                "value": True
+                },
+                {
+                "name": "pistons",
+                "value": True
+                },
+                {
+                "name": "frostwalker",
+                "value": False
+                },
+                {
+                "name": "fishingpole",
+                "value": True
+                }
+            ],
+            "INTERACTS": [
+                {
+                "BLOCK": "craftingtable",
+                "USE": True
+                },
+                {
+                "BLOCK": "enchantingtable",
+                "USE": False
+                },
+                {
+                "BLOCK": "itemframe",
+                "USE": False
+                },
+                {
+                "BLOCK": "armorstand",
+                "USE": False
+                },
+                {
+                "BLOCK": "anvil",
+                "USE": False
+                },
+                {
+                "BLOCK": "hopper",
+                "USE": True
+                },
+                {
+                "BLOCK": "lever",
+                "USE": False
+                },
+                {
+                "BLOCK": "button",
+                "USE": False
+                },
+                {
+                "BLOCK": "furnace",
+                "USE": False
+                },
+                {
+                "BLOCK": "door",
+                "USE": False
+                },
+                {
+                "BLOCK": "fencegate",
+                "USE": False
+                },
+                {
+                "BLOCK": "trapdoor",
+                "USE": False
+                },
+                {
+                "BLOCK": "pressureplate",
+                "USE": False
+                },
+                {
+                "BLOCK": "sign",
+                "USE": True
+                }
+            ],
+            "VEHICLES": [
+                {
+                "VEHICLE": "minecart",
+                "PLACE": False,
+                "DESTROY": False
+                },
+                {
+                "VEHICLE": "boat",
+                "PLACE": False,
+                "DESTROY": False
+                }
+            ],
+            "EXPLOSIONS": [
+                {
+                "EXPLOSION": "tnt",
+                "DAMAGE": True,
+                "DESTROY": False
+                },
+                {
+                "EXPLOSION": "creeper",
+                "DAMAGE": True,
+                "DESTROY": False
+                },
+                {
+                "EXPLOSION": "endercrystal",
+                "DAMAGE": True,
+                "DESTROY": False
+                },
+                {
+                "EXPLOSION": "fireball",
+                "DAMAGE": True,
+                "DESTROY": False
+                },
+                {
+                "EXPLOSION": "enderdragon",
+                "DAMAGE": True,
+                "DESTROY": False
+                },
+                {
+                "EXPLOSION": "otherexplosions",
+                "DAMAGE": True,
+                "DESTROY": False
+                }
+            ],
+            "MOBS": [],
+            "COMMANDS": [],
+            "EXCLUDED_BLOCKS": {
+                "PLACE": [],
+                "DESTROY": []
+            },
+            "DISALLOWED_ITEMS": [],
+            "DISALLOWED_BLOCKS": [],
+            "TEMPLATE": False
+        }
+
+flagDict = {
+    "place" : "block-place",
+    "destroy" : "block-break",
+    "pvp" : "pvp",
+    "expdrop" : "exp-drops",
+    "itemdrop" : "item-drop",
+    "itempickup" : "item-pickup",
+    "enderpearl" : "enderpearl",
+    "sleep" : "sleep",
+    "lighter" : "lighter",
+    "chests" : "chest-access",
+    "trappedchests" : None,
+    "waterflow" : "water-flow",
+    "lavaflow" : "lava-flow",
+    "otherliquidsflow" : None,
+    "leafdecay" : "leaf-decay",
+    "firespread" : "fire-spread",
+    "potionsplash" : "potion-splash",
+    "falldamage" : "fall-damage",
+    "cantp" : None,
+    "canspawn" : None,
+    "hunger" : "natural-hunger-drain",
+    "enderchests" : "chest-access",
+    "walldamage" : None,
+    "drown" : None,
+    "invincible" : "invincible",
+    "cactusdamage" : None,
+    "firedamage" : None,
+    "endermangrief" : "enderman-grief",
+    "enderdragonblockdamage" : "enderdragon-block-damage",
+    "hidelocation" : None,
+    "hideflags" : None,
+    "hidemembers" : None,
+    "hideregion" : None,
+    "icemelt" : "ice-melt",
+    "exit" : "exit",
+    "enter" : "entry",
+    "vinesgrowth" : "vine-growth",
+    "sendchat" : "send-chat",
+    "trample" : "block-trampling",
+    "shulkerboxes" : "chest-access",
+    "pistons" : "pistons",
+    "frostwalker" : "frosted-ice-form",
+    "fishingpole" : None,
+    "craftingtable" : "use",
+    "enchantingtable" : "use",
+    "itemframe" : "use",
+    "armorstand" : "use",
+    "anvil" : "use",
+    "hopper" : "use",
+    "lever" : "use",
+    "button" : "use",
+    "furnace" : "use",
+    "door" : "use",
+    "fencegate" : "use",
+    "trapdoor" : "use",
+    "pressurplate" : "use",
+    "sign" : "use"
+}
+
+vehicleDict = {
+    "minecart" : {
+        "place" : "vehicle-place",
+        "destroy" : "vehicle-destroy"
+    },
+    "boat" : {
+        "place" : "vehicle-place",
+        "destroy" : "vehicle-destroy"
+    }
+}
+
+explosionDict = {
+    "tnt" : {
+        "DAMAGE" : "tnt",
+        "DESTROY" : "tnt"
+    },
+    "creeper" : {
+        "DAMAGE" : "creeper-explosion",
+        "DESTROY" : "creeper-explosion" 
+    },
+    "fireball" : {
+        "DAMAGE" : "ghast-fireball",
+        "DESTROY" : "ghast-fireball" 
+    },
+    "enderdragon" : {
+        "DAMAGE" : "enderdragon-block-damage",
+        "DESTROY" : "enderdragon-block-damage" 
+    },
+    "otherexplosions" : {
+        "DAMAGE" : "other-explosion",
+        "DESTROY" : "other-explosion" 
+    },
+}
---
+++ WG_UG_converter.py
@@ -0,0 +1,169 @@
+import yaml
+import uuid
+import requests
+import UG
+import json
+
+def getUsername(uuid):
+    # Just for testing
+    # uuid = "f6322e93-4c9b-4a9f-bc3c-7ea6b8600d6f"
+    try:
+        response = requests.get(f"https://playerdb.co/api/player/minecraft/{uuid}")
+        jsonDecode = response.json()
+        return(jsonDecode["data"]["player"]["username"])
+    except:
+        print("Somethin went wrong, can't fetch the username!")
+        return -1
+
+
+if __name__== "__main__":
+    regions = None
+    regionIndex = {}
+    with open("regions.yml", 'r') as stream:
+        try:
+            regions = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    for region in regions["regions"]:
+        # print(regions["regions"][region])
+        if region == "__global__":
+            continue
+        newRegion = UG.UG()
+
+        # Region name
+        newRegion.data["NAME"] = region
+        # First anker point of the region
+        newRegion.data["FIRST_POINT"]["x"] = regions["regions"][region]["min"]["x"]
+        newRegion.data["FIRST_POINT"]["y"] = regions["regions"][region]["min"]["y"]
+        newRegion.data["FIRST_POINT"]["z"] = regions["regions"][region]["min"]["z"]
+        # Second anker point of the region
+        newRegion.data["SECOND_POINT"]["x"] = regions["regions"][region]["max"]["x"]
+        newRegion.data["SECOND_POINT"]["y"] = regions["regions"][region]["max"]["y"]
+        newRegion.data["SECOND_POINT"]["z"] = regions["regions"][region]["max"]["z"]
+        # UUID for the region
+        newRegion.data["ID"] = str(uuid.uuid1())
+        # Greeting/farewell message
+        try:
+            newRegion.data["GREETING_MESSAGE"] = regions["regions"][region]["flags"]["greeting"]
+        except:
+            newRegion.data["GREETING_MESSAGE"] = ""
+        try:
+            newRegion.data["FAREWELL_MESSAGE"] = regions["regions"][region]["flags"]["farewell"]
+        except:
+            newRegion.data["FAREWELL_MESSAGE"] = ""
+
+        # Adding the region owners and members, if there is a parent region in WorldGuard, the "superowners" are also added
+        try:
+            parent = regions["regions"][region]["parent"]
+
+            for superowner in regions["regions"][parent]["owners"]["unique-ids"]:
+                newOner = {  
+                "UUID" : "",
+                "USERNAME" : "",
+                "ROLE" : "OWNER"
+                }
+                newOner["UUID"] = superowner
+                newOner["USERNAME"] = getUsername(superowner)
+
+                newRegion.data["MEMBERS"].append(newOner)
+        except:
+            print("No parent region found!")
+        
+        try:
+            for owner in regions["regions"][region]["owners"]["unique-ids"]:
+                newOner = {  
+                    "UUID" : "",
+                    "USERNAME" : "",
+                    "ROLE" : "OWNER"
+                }
+                newOner["UUID"] = owner
+                newOner["USERNAME"] = getUsername(owner)
+
+                
+                newRegion.data["MEMBERS"].append(newOner)
+        except:
+            print("Region has no Owner or just a Superowner")
+        try:
+            for member in regions["regions"][region]["members"]["unique-ids"]:
+                newMember = {  
+                    "UUID" : "",
+                    "USERNAME" : "",
+                    "ROLE" : "MEMBER"
+                }
+                newOner["UUID"] = member
+                newOner["USERNAME"] = getUsername(member)
+
+                
+                newRegion.data["MEMBERS"].append(newMember)
+        except:
+            print("Region has no Member")
+            
+        # Set all the flags if a flag is not set it gets the default defined in UG.py
+        for flag in newRegion.data["FLAGS"]:
+            try:
+                if regions["regions"][region]["flags"][UG.flagDict[flag["name"]]] == "deny":
+                    flag["value"] = False
+                elif regions["regions"][region]["flags"][UG.flagDict[flag["name"]]] == "allow":
+                    flag["value"] = True
+            except:
+                pass
+
+        # Set all the INTERACT flags
+        for interact in newRegion.data["INTERACTS"]:
+            try: 
+                if regions["regions"][region]["flags"][UG.flagDict[interact["BLOCK"]]] == "deny":
+                    interact["USE"] = False
+                elif regions["regions"][region]["flags"][UG.flagDict[interact["BLOCK"]]] == "allow":
+                    interact["USE"] = True    
+            except:
+                pass
+
+        # Set all the VEHICLES flags
+        for vehicle in newRegion.data["VEHICLES"]:
+            try:
+                if regions["regions"][region]["flags"][UG.vehicleDict[vehicle["VEHICLE"]["place"]]] == "deny":
+                    vehicle["PLACE"] = False
+                elif regions["regions"][region]["flags"][UG.vehicleDict[vehicle["VEHICLE"]["place"]]] == "allow":
+                    vehicle["PLACE"] = True
+                if regions["regions"][region]["flags"][UG.vehicleDict[vehicle["VEHICLE"]["destroy"]]] == "deny":
+                    vehicle["DESTROY"] = False    
+                elif regions["regions"][region]["flags"][UG.vehicleDict[vehicle["VEHICLE"]["destroy"]]] == "allow":
+                    vehicle["DESTROY"] = True
+            except:
+                pass
+
+        # Set all the EXPLOSIONS flags
+        for explosion in newRegion.data["EXPLOSIONS"]:
+            try:
+                if regions["regions"][region]["flags"][UG.explosionDict[explosion["EXPLOSION"]["DAMAGE"]]] == "deny":
+                    vehicle["DAMAGE"] = False
+                elif regions["regions"][region]["flags"][UG.explosionDict[explosion["EXPLOSION"]["DAMAGE"]]] == "allow":
+                    vehicle["DAMAGE"] = True
+                if regions["regions"][region]["flags"][UG.explosionDict[explosion["EXPLOSION"]["DESTROY"]]] == "deny":
+                    vehicle["DAMAGE"] = False
+                elif regions["regions"][region]["flags"][UG.explosionDict[explosion["EXPLOSION"]["DESTROY"]]] == "allow":
+                    vehicle["DESTROY"] = True
+            except:
+                pass
+
+
+        jsonDump = json.dumps(newRegion.data, indent=4)
+        fname = newRegion.data["ID"]
+        f = open(f"{fname}.json", "w")
+        f.write(jsonDump)
+        f.close()
+        
+        regionIndex[newRegion.data["NAME"]] = newRegion.data["ID"]
+
+    jsonDump = json.dumps(regionIndex, indent=4)
+    f = open("index.json", "w")
+    f.write(jsonDump)
+    f.close()
+
+    print(uuid.uuid1())
+
+
+
+
+# print(regions)
