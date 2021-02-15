d88651d7a0a261a4da3983430bb920bbbdde616b
SidewinderDX <d_kohl@hotmail.de>
2021-02-15 18:54:27 +0100

    Update of the Version Number (2.32 -> 2.33)

--- UG.py
+++ UG.py
@@ -24,7 +24,7 @@ class UG:
             "EFFECTS": [],
             "SOLD": False,
             "VALUE": None,
-            "VERSION": 2.32,
+            "VERSION": 2.33,
             "ID": "",
             "TYPE": "LOCAL",
             "NAME": "",
